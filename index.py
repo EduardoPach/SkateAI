92415dded2e4e762a948670da405823a1dac7062
Eduardo <eduardo.pacheco@casai.com>
2022-03-16 23:06:59 -0300

    index file for app
    main file to run the trimm app

---
+++ index.py
@@ -0,0 +1,34 @@
+from dash import html, dcc 
+import dash_bootstrap_components as dbc
+
+from app import app 
+from video_player import *
+
+app.layout = dbc.Container(
+    children=[
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dcc.Slider(
+                            id="slider-playback-rate",
+                            min=0.25,
+                            max=5,
+                            step=None,
+                            marks={i: str(i) + "x" for i in [0.25, 1, 2, 5]},
+                            value=1
+                        ),
+                        controls
+                    ],
+                )
+            ]
+        )
+    ],
+    fluid=True,
+    style={"padding": "60px 60px"}
+
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
