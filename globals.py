b5809480b4660db5e2f3b50493355afd10f3367f
Eduardo <eduardo.pacheco@casai.com>
2022-03-18 00:00:40 -0300

    Update globals
    Added some options that are in the widgets in the controls

--- globals.py
+++ globals.py
@@ -1,3 +1,5 @@
 import os 
 
 VIDEO_PATH = [os.path.join("videos", i) for i in os.listdir("videos")]
+STANCES = ["regular", "switch", "fakie", "nollie"]
+BODY_ROTATION_TYPE = ["frontside", "backside", "none"]
