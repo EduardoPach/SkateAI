84dd0d8ebaef39d761c9298ac8fff04ecd0581b6
Eduardo <eduardo.pacheco@casai.com>
2022-05-20 00:16:23 -0300

    changed VIDEO_PATH to store get_videos_url output

--- globals.py
+++ globals.py
@@ -1,6 +1,8 @@
 import os 
 
-VIDEO_PATH = [os.path.join("videos", i) for i in os.listdir("videos")]
+import utils
+
+VIDEO_PATH = utils.get_videos_url(["https://www.youtube.com/playlist?list=PLmxvVi4Ors7aqc726ngHq1SwTrBGPjCSN"]) 
 STANCES = ["regular", "switch", "fakie", "nollie"]
 ROTATION_TYPE = ["none", "backside", "frontside"]
 FLIP_TYPE = ["none", "kickflip", "heelflip"]
