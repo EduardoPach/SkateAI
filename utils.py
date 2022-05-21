4c3c058728b06e3731d5702d598c57ab2cd839d4
Eduardo <eduardo.pacheco@casai.com>
2022-05-21 01:05:34 -0300

    Helper functions to deal with cutting tricks

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 import pytube as yt
 
 def get_videos_url(urls: list) -> dict[str, str]:
@@ -20,4 +23,33 @@ def get_videos_url(urls: list) -> dict[str, str]:
         playlist = yt.Playlist(url)
         for title, video_url in zip([video.title for video in playlist.videos],playlist.video_urls):
             data[title] = video_url
+    return data
+
+def update_cuts(data: dict, video_url: str, cut_name: str, start_time: int, end_time: int, trick_info: dict) -> dict:
+    if video_url not in data:
+        data[video_url] = {
+            cut_name: {
+                "interval": [start_time, end_time],
+                "trick_info": trick_info
+            }
+        }
+    else:
+        data[video_url][cut_name] = {"interval": [start_time, end_time], "trick_info": trick_info}
+    return data
+
+def delete_cuts(data: dict, video_url: str, current_cut: str) -> dict:
+    del data[video_url][current_cut]
+    return data
+
+def get_cuts_data() -> dict:
+    DIR = "batb11"
+    JSON_PATH = f"{DIR}/tricks_cut.json"
+    if not os.path.exists(DIR):
+        os.mkdir(DIR)
+
+    if not os.path.exists(JSON_PATH):
+        data = {}
+    else:
+        with open(JSON_PATH, 'r') as f:
+            data = json.load(f)
     return data
