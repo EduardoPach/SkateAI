3ee70a22bd75247f839bcdd54be663f72f820a54
Eduardo <eduardo.pacheco@casai.com>
2022-06-14 19:05:11 -0300

    Won't download videos that were already downloaded

--- generate_data.py
+++ generate_data.py
@@ -31,14 +31,17 @@ def main() -> None:
 
                 print(f"STARTING {cut_name}")
                 start, end = cut_info["interval"]
-                print("CUTTING VIDEO: ", end='')
-                with mo.VideoFileClip(f"{path}/fullvideo.mp4") as f:
-                    clip = f.subclip(start, end)
-                    video_path = f"{path}/{cut_name.replace(' ', '_').lower()}.mp4"
-                    clip.write_videofile(video_path)
-                print("Success!")
-                
-                utils.update_metadata(video_path, video_title, url, [start, end], cut_info["trick_info"])
+                video_path = f"{path}/{cut_name.replace(' ', '_').lower()}.mp4" 
+                if os.path.exists(video_path):
+                    print("ALREADY EXISTS!\n")
+                else:
+                    print("CUTTING VIDEO: ", end='')
+                    with mo.VideoFileClip(f"{path}/fullvideo.mp4") as f:
+                        clip = f.subclip(start, end)
+                        clip.write_videofile(video_path)
+                    print("Success!")
+                    
+                    utils.update_metadata(video_path, video_title, url, [start, end], cut_info["trick_info"])
                 pbar.update(1)
                 print("")
             os.remove(f"{path}/fullvideo.mp4")
