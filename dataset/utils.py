311716c3dc17f6fe438385dc3a36a81c6a96c082
EduardoPach <eduardo.pach@hotmail.com>
2022-09-20 02:18:08 -0300

    Dataset returns list of PILs and new VideoToTensor

--- dataset/utils.py
+++ dataset/utils.py
@@ -1,9 +1,13 @@
+from PIL import Image
+
 import cv2
+import torch
 import numpy as np
 import pandas as pd
+import torchvision.transforms.functional as TF
 
 
-def get_video(video_file: str) -> np.array:
+def get_video(video_file: str) -> list:
     video_reader = cv2.VideoCapture(video_file)
     frames = []
     while True:
@@ -11,6 +15,11 @@ def get_video(video_file: str) -> np.array:
         if not success:
             break
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame.astype("uint8"))
         frames.append(frame)
     video_reader.release()
-    return np.array(frames)
+    return frames
+
+class VideoToTensor:
+    def __call__(self, imgs: list) -> torch.FloatTensor:
+        return torch.stack([TF.pil_to_tensor(img) for img in imgs])
