9d29d0172bea6b86859b1c3457667358785c6383
Eduardo <eduardo.pach@hotmail.com>
2022-12-24 17:20:53 -0300

    Added padding to videos

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,27 +1,40 @@
 import os
+from typing import Union
 from pathlib import Path
 
+import torch
 import pandas as pd
 from torch.utils.data import Dataset
 
-import const
-import utils
+from dataset import const
+from dataset import utils
 
 class TricksDataset(Dataset):
-    def __init__(self, csv_file: Path, root_dir: Path, transform: list=None) -> None:
+    def __init__(self, csv_file: Union[Path, str], root_dir: Union[Path, str], max_frames: int, transform: list=None) -> None:
         self.video_dir = root_dir
         self.transform = transform
         self.metadata = pd.read_csv(csv_file)
+        self.max_frames = max_frames
     
     def __len__(self) -> int:
         return len(self.metadata)
     
     def __getitem__(self, index):
-        video_path = os.path.join(self.video_dir, self.metadata.iloc[index, 0])
-        labels = self.metadata.loc[self.metadata["video_file"]==video_path, const.LABELS].to_numpy().astype(int)
-        frames = utils.get_video(video_path)
+        video_file = self.metadata.iloc[index, 0]
+        video_path = os.path.join(self.video_dir, video_file)
+        labels = self.metadata.loc[index, const.LABELS].to_numpy().astype(int)
+        video = utils.get_video(video_path)
+        video = utils.VideoToTensor()(video)
+        F, C, H, W = video.shape
+
+        if F < self.max_frames:
+            frames_diff = int(self.max_frames - F)
+            padding = torch.zeros(frames_diff, C, H, W)
+            video = torch.cat([padding, video])
+        elif F > self.max_frames:
+            video = video[:self.max_frames, ...]
 
         if self.transform:
-            frames = self.transform(frames)
+            video = self.transform(video)
 
-        return frames, labels
+        return video, labels
