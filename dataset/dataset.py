6e13e4fce767e1b6ab874991b14648d756a148eb
Eduardo <eduardo.pach@hotmail.com>
2022-12-24 17:32:27 -0300

    Changed max frames to optional

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -3,14 +3,16 @@ from typing import Union
 from pathlib import Path
 
 import torch
+import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
+from torchvision.transforms import Compose
 
 from dataset import const
 from dataset import utils
 
 class TricksDataset(Dataset):
-    def __init__(self, csv_file: Union[Path, str], root_dir: Union[Path, str], max_frames: int, transform: list=None) -> None:
+    def __init__(self, csv_file: Union[Path, str], root_dir: Union[Path, str], max_frames: Union[None, int], transform: Union[None, Compose]) -> None:
         self.video_dir = root_dir
         self.transform = transform
         self.metadata = pd.read_csv(csv_file)
@@ -19,20 +21,21 @@ class TricksDataset(Dataset):
     def __len__(self) -> int:
         return len(self.metadata)
     
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> tuple[torch.Tensor, np.array]:
         video_file = self.metadata.iloc[index, 0]
         video_path = os.path.join(self.video_dir, video_file)
         labels = self.metadata.loc[index, const.LABELS].to_numpy().astype(int)
         video = utils.get_video(video_path)
         video = utils.VideoToTensor()(video)
-        F, C, H, W = video.shape
 
-        if F < self.max_frames:
-            frames_diff = int(self.max_frames - F)
-            padding = torch.zeros(frames_diff, C, H, W)
-            video = torch.cat([padding, video])
-        elif F > self.max_frames:
-            video = video[:self.max_frames, ...]
+        if self.max_frames:
+            F, C, H, W = video.shape
+            if F < self.max_frames:
+                frames_diff = int(self.max_frames - F)
+                padding = torch.zeros(frames_diff, C, H, W)
+                video = torch.cat([padding, video])
+            elif F > self.max_frames:
+                video = video[:self.max_frames, ...]
 
         if self.transform:
             video = self.transform(video)
