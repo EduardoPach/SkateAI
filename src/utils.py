c9b034e65bc0c3b76033351073ecb94596a8afad
Eduardo <eduardo.pach@hotmail.com>
2022-12-26 00:45:46 -0300

    Finished train_fn

--- src/utils.py
+++ src/utils.py
@@ -4,22 +4,25 @@ import torch.nn as nn
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
-def train_fn(loader: DataLoader, model: nn.Module, optimizer: Optimizer, loss_fns: list[nn.Module], device: "str"):
+def train_fn(loader: DataLoader, model: nn.Module, optimizer: Optimizer, loss_fns: dict[str, nn.Module], device: "str"):
     loop = tqdm(loader)
 
     for batch_idx, (data, targets) in enumerate(loop):
         data = data.to(device=device)
-        targets = targets.float().unsqueeze(1).to(device=device)
+        targets = {targets[key].to(device=device) for key in targets.keys()}
 
         # forward
-        with torch.cuda.amp.autocast():
-            predictions = model(data)
-            loss = loss_fn(predictions, targets)
+        predictions = model(data)
+        loss_dict = {}
+        loss_total = 0
+        for key in predictions.keys():
+            loss_dict[key] = loss_fns[key](predictions[key], targets[key])
+            loss_total += loss_dict[key]
 
         # backward
         optimizer.zero_grad()
-        loss.backward()
+        loss_total.backward()
         optimizer.step()
 
         # update tqdm loop
-        loop.set_postfix(loss=loss.item())
+        loop.set_postfix(loss_total=loss_total.item())
