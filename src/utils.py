35ded28f743b99a34bf8960e7e3e85730d2b866f
Eduardo <eduardo.pach@hotmail.com>
2022-12-26 23:31:31 -0300

    Added save and load checkpoints

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+from typing import Union
+
 import tqdm
 import torch
 import torch.nn as nn
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
+
+from dataset import TricksDataset
 
 def train_fn(loader: DataLoader, model: nn.Module, optimizer: Optimizer, loss_fns: dict[str, nn.Module], device: "str"):
     loop = tqdm(loader)
@@ -26,3 +32,59 @@ def train_fn(loader: DataLoader, model: nn.Module, optimizer: Optimizer, loss_fn
 
         # update tqdm loop
         loop.set_postfix(loss_total=loss_total.item())
+
+def get_loaders(
+    train_csv: Union[str, Path],
+    val_csv: Union[str, Path],
+    root_dir: Union[str, Path],
+    max_frames: int,
+    batch_size: int,
+    train_transform: Compose,
+    val_transform: Compose,
+    num_workers: int=4,
+    pin_memory: bool=True,
+) -> tuple[DataLoader, DataLoader]:
+
+    train_ds = TricksDataset(
+        csv_file=train_csv,
+        root_dir=root_dir,
+        max_frames=max_frames,
+        transform=train_transform
+    )
+
+    train_loader = DataLoader(
+        dataset=train_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True,
+    )
+
+    val_ds = TricksDataset(
+        csv_file=train_csv,
+        root_dir=root_dir,
+        max_frames=max_frames,
+        transform=train_transform
+    )
+
+    val_loader = DataLoader(
+        dataset=val_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=False,
+    )
+
+    return train_loader, val_loader
+    
+
+def save_checkpoint(state, filename="my_checkpoint.pth.tar") -> None:
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint, model) -> None:
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+
+def check_performance() -> None:
+    pass
