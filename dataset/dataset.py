6229800acb43421e3b7335e37234b380bb9e11e1
Eduardo <eduardo.pach@hotmail.com>
2023-01-31 16:44:43 -0300

    Included label encoder and new dir format

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -9,24 +9,36 @@ import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
 from torchvision.transforms import Compose
+from sklearn.preprocessing import OrdinalEncoder
 
 from dataset import const
 from dataset import utils
 
 class TricksDataset(Dataset):
-    def __init__(self, csv_file: Union[Path, str], root_dir: Union[Path, str], max_frames: Union[None, int]=None, transform: Union[None, Compose]=None) -> None:
-        self.video_dir = root_dir
+    def __init__(
+        self, 
+        csv_file: Union[Path, str], 
+        root_dir: Union[Path, str], 
+        max_frames: Union[None, int]=None, 
+        transform: Union[None, Compose]=None,
+        label_enconder: OrdinalEncoder=None
+
+    ) -> None:
+        self.root_dir = root_dir
         self.transform = transform
         self.metadata = pd.read_csv(csv_file)
         self.max_frames = max_frames
-    
+        self.label_encoder = label_enconder
+
     def __len__(self) -> int:
         return len(self.metadata)
     
     def __getitem__(self, index: int) -> tuple[torch.Tensor, dict[str, int]]:
         video_file = self.metadata.iloc[index, 0]
-        video_path = os.path.join(self.video_dir, video_file)
-        labels = {col: self.metadata.loc[index, col] for col in const.LABELS}
+        video_dir = self.metadata.iloc[index, 1]
+        video_path = os.path.join(self.root_dir, video_dir, video_file)
+        encoded = self.label_encoder.transform(self.metadata[const.LABELS])
+        labels = {col: encoded.loc[index, col] for col in const.LABELS}
         video = utils.get_video(video_path)
         video = utils.VideoToTensor()(video)
 
