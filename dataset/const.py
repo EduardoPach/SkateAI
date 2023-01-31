462568c1d7e3bd2570b4945f678f2b3762599672
Eduardo <eduardo.pach@hotmail.com>
2023-01-31 16:44:15 -0300

    Changed labels that will be used

--- dataset/const.py
+++ dataset/const.py
@@ -1,10 +1,11 @@
 LABELS = [
-    "body_rotation_type",
-    "body_rotation_number",
-    "board_rotation_type",
-    "board_rotation_number",
-    "flip_type",
-    "flip_number",
+    # "body_rotation_type",
+    # "body_rotation_number",
+    # "board_rotation_type",
+    # "board_rotation_number",
+    # "flip_type",
+    # "flip_number",
+    "trick_name",
     "landed",
     "stance"
 ]
