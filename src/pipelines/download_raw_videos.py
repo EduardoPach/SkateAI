75fdff5e6ab743b6f0cb729343cb9b363e537855
EduardoPach <eduardo.pach@hotmail.com>
2023-04-10 16:10:06 -0300

    Refactored raw video pipeline

--- src/pipelines/download_raw_videos.py
+++ src/pipelines/download_raw_videos.py
@@ -1,89 +1,86 @@
 from __future__ import annotations
 
-import os
-import shutil
+import argparse
+from typing import Callable, Union
 
-import tqdm
-import boto3
-import pytube as yt
-from dotenv import load_dotenv
+import pandas as pd
 
 import utils
 from src.labeling_tool.const import VIDEOS_PER_SOURCE
+from src.pipelines.const import AWS_S3_RAW_VIDEOS_DIR, LOCAL_RAW_VIDEOS_DIR
 
-load_dotenv()
-
-def upload_mp4_to_s3(filepath: str, **kwargs) -> None:
-    """Upload mp4 file to S3
-
-    Parameters
-    ----------
-    filepath : str
-        filepath on local machine to upload to S3
-    """
-    BUCKET_NAME = os.environ["S3_BUCKET"]
-    s3 = boto3.client("s3")
-
-    # Get the filename from the filepath
-    filename = os.path.basename(filepath)
-
-    # Upload the file to S3
-    try:
-        s3.upload_file(filepath, BUCKET_NAME, filename, ExtraArgs={"Metadata": kwargs})
-        print(f"Successfully uploaded {filename} to S3 bucket {BUCKET_NAME}\n")
-    except Exception as e:
-        print(f"Error uploading {filename} to S3 bucket {BUCKET_NAME}: {e}\n")
-    
-    os.remove(filepath)
+def retry(n: int, func: Callable, *args, **kwargs) -> tuple[Union[dict, None], bool]:
+    for _ in range(n):
+        try:
+            return func(*args, **kwargs), True
+        except Exception as e:
+            pass
+    return None, False
 
 
-def download_video(video_title: str, video_url: str, source: str) -> None:
-    """Download video from YouTube and upload to S3
+def main(ignore_source: list[str], store: str) -> None:
+    """Pipeline that downloads raw videos from Youtube and store
+    them on the adaquate location.
 
     Parameters
     ----------
-    video_title : str
-        video title
-    video_url : str
-        video url
-    source : str
-        video source
+    ignore_source : list[str], optional
+        Video sources that should be ignored, by default None
+    store : str, optional
+        How to store the raw videos. Should be one of the options
+        's3', 'local', 'both', by default 'both'
     """
-    video = yt.YouTube(video_url)
-    video_length = video.length
-    video_title_parsed = utils.parse_video_title(video_title)
-    video.streams.filter(
-        res="480p",
-        file_extension='mp4',
-        type="video",
-        only_video=True
-    )[0].download(output_path=".", filename=f"{video_title_parsed}.mp4")
-
-    upload_mp4_to_s3(
-        f"./{video_title_parsed}.mp4",
-        video_title=video_title,
-        video_url=video_url,
-        video_source=source,
-        video_length=str(video_length)
-    )
-
-        
-
-def main(ignore_source: list[str]=None) -> None:
+    metadata = []
     for source in VIDEOS_PER_SOURCE.keys():
         if source and source in ignore_source:
             continue
         for video_title, video_url in VIDEOS_PER_SOURCE[source].items():
-            tries = 0
-            while tries < 10: # Workaround pytube issue
-                try:
-                    download_video(video_title, video_url, source)
-                    break
-                except Exception as e:
-                    tries +=1
-                    if tries==10:
-                        print(f"Skipping {video_title} due to error: ", e)
-                    
+            video_dict, success = retry(
+                n=10, 
+                func=utils.download_video, 
+                video_title=video_title, 
+                video_url=video_url, 
+                source=source, 
+                directory=LOCAL_RAW_VIDEOS_DIR
+            )
+            if not success:
+                print(f"Failed to download {video_title} from {source}")
+                continue
+            
+            if store=="local":
+                continue
+            remove_local = True if store=="s3" else False 
+            utils.upload_mp4_to_s3(
+                directory=AWS_S3_RAW_VIDEOS_DIR, 
+                remove_local=remove_local, 
+                **video_dict
+            )
+    
+    metadata_df = pd.DataFrame(metadata)
+    if store=="local":
+        metadata_df.to_csv(f"{LOCAL_RAW_VIDEOS_DIR}/metadata.csv", index=False)
+    elif store=="s3":
+        utils.upload_df_to_s3(metadata_df, f"{AWS_S3_RAW_VIDEOS_DIR}/metadata.csv")
+    else:
+        metadata_df.to_csv(f"{LOCAL_RAW_VIDEOS_DIR}/metadata.csv", index=False)
+        utils.upload_df_to_s3(metadata_df, f"{AWS_S3_RAW_VIDEOS_DIR}/metadata.csv")            
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Command line downloading the data")
+
+    parser.add_argument(
+        "--ignore-source",
+        nargs="+",
+        type=str,
+        help="Video sources that should be ignored",
+        default=[]
+    )
+
+    parser.add_argument(
+        "--store",
+        type=str,
+        help="How to store the raw videos. Should be one of the options 's3', 'local', 'both'",
+        default="both"
+    )
+
+    main(**vars(parser.parse_args()))
